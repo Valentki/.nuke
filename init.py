0a1a91397059dd2c19493fc53291cbdcdcec799d
Valentki <kalvin.irawan@gmail.com>
2019-08-23 17:45:47 +0700

    Update init.py
    Initial init.py file

--- init.py
+++ init.py
@@ -1,1 +1,38 @@
+## -------------------------------------------------------------------------------------
+# init.py
+# Version: 1.0.0 (Major,Minor,Revision)
+# Last Updated: August 22nd, 2019
+## -------------------------------------------------------------------------------------
+
+
+
+## Per show plugin path ---------------------------------------------------
+nuke.pluginAddPath('./gizmos')
+nuke.pluginAddPath('./python')
+
+
+## Cryptomatte -------------------------------------------------------------------------
+#
+#  Copyright (c) 2014, 2015, 2016, 2017 Psyop Media Company, LLC
+#  See license.txt
+#
+
+
+nuke.pluginAddPath('./python/Cryptomatte')
+
+import cryptomatte_utilities
+cryptomatte_utilities.setup_cryptomatte()
+
+
+
+## Xtools ------------------------------------------------------------------------------
+nuke.pluginAddPath('./python/X_Tools')
+nuke.pluginAddPath('./python/X_Tools/Icons')
+nuke.pluginAddPath('./python/X_Tools/Gizmos')
+
+
+
+## KeenTools ---------------------------------------------------------------------------
+nuke.pluginAddPath('./python/KeenTools')
+
 
